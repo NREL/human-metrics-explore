d4b5190438b20ee3cc06399c63ffb9e47e7fe3f7
K. Shankari <shankari@eecs.berkeley.edu>
2022-01-12 10:02:54 -0800

    Simplify the setup process for local installs to use a prefixed miniconda install
    Before this, we were checking to see if conda was already installed and was the
    correct version before asking people to install the correct version of conda.
    However, if conda was not installed at all, we got errors while running
    `check_for_conda`.
    
    ```
    check_for_conda.sh: line 2: conda: command not found
    ```
    
    And while activating, we assumed that conda was installed
    with the correct prefix anyway.
    
    So let's just use the prefix based method for everything for now, which
    simplifies the codebase.
    - We no longer need check_for_conda
    - We can just check for the file existence in activate_conda
    - We can use activate_conda in both setup_notebook and activate_notebook
    
    TODO: Unify with the e-mission server code and the public dashboard code and
    create a standard module that all of them can use.

--- setup/activate_notebook.sh
+++ setup/activate_notebook.sh
@@ -1,8 +1,3 @@
-EXP_CONDA_VER=4.8.3
-
-INSTALL_PREFIX=$HOME/miniconda-$EXP_CONDA_VER
-SOURCE_SCRIPT="$HOME/miniconda-$EXP_CONDA_VER/etc/profile.d/conda.sh"
-
-source $SOURCE_SCRIPT
+source setup/activate_conda.sh
 conda activate aces_metrics
 jupyter notebook
